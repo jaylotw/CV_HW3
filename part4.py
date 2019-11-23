f6ef72430382ca4b72ea7c455da4e1ded41a85ca
Jay Lo <jaylo.eed03@g2.nctu.edu.tw>
2019-11-23 14:07:31 +0800

    test

--- part4.py
+++ part4.py
@@ -1,10 +1,11 @@
+import argparse
 import numpy as np
 import cv2
 import sys
 
-def main(ref_image,template,video):
-    ref_image = cv2.imread(ref_image)  ## load gray if you need.
-    template = cv2.imread(template)  ## load gray if you need.
+def main(ref_image, template ,video):
+    ref_image = cv2.imread(ref_imag, cv2.IMREAD_GRAYSCALE)  ## load gray if you need.
+    template = cv2.imread(template, cv2.IMREAD_GRAYSCALE)  ## load gray if you need.
     video = cv2.VideoCapture(video)
     film_h, film_w = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT)), int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
     film_fps = video.get(cv2.CAP_PROP_FPS)
@@ -29,8 +30,10 @@ def main(ref_image,template,video):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--video_path", default='./ar_marker.mp4')
     ## you should not change this part
     ref_path = './input/sychien.jpg'
     template_path = './input/marker.png'
-    video_path = sys.argv[1]  ## path to ar_marker.mp4
+    video_path = parser.parse_args()  ## path to ar_marker.mp4
     main(ref_path,template_path,video_path)
