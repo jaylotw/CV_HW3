ce66e2f4b38766dc9343afb9b215412e9bb853b7
Jay Lo <jaylo.eed03@g2.nctu.edu.tw>
2019-11-25 13:37:31 +0800

    ORB

--- part4.py
+++ part4.py
@@ -3,9 +3,9 @@ import numpy as np
 import cv2
 import sys
 
-debug = False
+debug = True
 MIN_MATCH_COUNT = 15
-DET_METHOD = 'SIFT'
+DET_METHOD = 'ORB'
 
 def main(ref_image, template ,video):
     ref_image = cv2.imread(ref_image)  ## load gray if you need.
@@ -25,13 +25,17 @@ def main(ref_image, template ,video):
     elif DET_METHOD == 'SIFT':
         detector = cv2.xfeatures2d.SIFT_create()
     elif DET_METHOD == 'ORB':
-        detector = cv2.ORB_create()
+        detector = cv2.ORB_create(nfeatures=100000, scoreType=cv2.ORB_FAST_SCORE)
         
     if debug:
         print('detector: {}'.format(DET_METHOD))
 
     # Brute-force matcher
-    matcher = cv2.BFMatcher()
+    if DET_METHOD == 'ORB':
+        matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    else:
+        matcher = cv2.BFMatcher()
+    
     # Find the keypoints and descriptors of marker
     kp_m, des_m = detector.detectAndCompute(template, None)    
     
@@ -44,17 +48,25 @@ def main(ref_image, template ,video):
             # Find the keypoints and descriptors of frame
             kp_f, des_f = detector.detectAndCompute(frame, None)
             
+            # Match the keypoints
+            if DET_METHOD == 'ORB':
+                matches = matcher.match(des_m, des_f)
+                matches = sorted(matches, key = lambda x:x.distance)
+            else:
+                matches =  matcher.knnMatch(des_m, des_f, k=2)
+                
             if debug:
                 print('#keypoints in marker: %d, frame: %d' % (len(des_m), len(des_f)))
-            
-            # Match the keypoints
-            matches =  matcher.knnMatch(des_m, des_f, k=2)
+                print('#matches: %d' % (len(matches)))
                 
             # Store all the good matches as per Lowe's ratio test
-            good = list()
-            for (m, n) in matches:
-                if m.distance < 0.75 * n.distance:
-                    good.append(m)
+            if DET_METHOD == 'ORB':
+                good = matches[:int(len(matches) * 0.2)]
+            else:
+                good = list()
+                for (m, n) in matches:
+                    if m.distance < 0.75 * n.distance:
+                        good.append(m)
             
             # Run RANSAC and get the homography
             if len(good) > MIN_MATCH_COUNT:
@@ -73,7 +85,7 @@ def main(ref_image, template ,video):
             
                 img_match = cv2.drawMatches(template, kp_m, frame, kp_f, good, None, **draw_params)
             
-                print('Final matches: %d' % (len(good)))
+                print('#good matches: %d' % (len(good)))
                 cv2.imwrite("./debug/{}/match_{:03d}_{:03d}.jpg".format(DET_METHOD, i, len(good)), img_match)
 
             # Warp the reference image and paste it on the video frame
